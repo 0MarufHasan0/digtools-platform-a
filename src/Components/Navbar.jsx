e8985a40532bfdbd9819131cdb107defb6fed4c5
0MarufHasan0 <marufhasan11.11.2323@gmail.com>
2026-03-30 10:31:19 +0600

    all assest and navbar  done

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { ShoppingCart } from 'lucide-react';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+           <div className="navbar container mx-auto">
+      <div className="navbar-start">
+        <a className=" text-[#4F39F6] font-bold  text-4xl">DigTools</a>
+      </div>
+      <div className="navbar-center hidden md:flex">
+        <ul className="menu menu-horizontal gap-10 px-1 text-lg">
+          <li>
+            <a>Products</a>
+          </li>
+          <li>
+            <a>Features</a>
+          </li>
+          <li>
+            <a>Pricing</a>
+          </li>
+          <li>
+            <a>Testimonials</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end gap-5">
+
+         <a className="btn border-none">  <ShoppingCart /></a>
+        
+        <a className="btn border-none">Login</a>
+        <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white"> Get in Touch</a>
+      </div>
+    </div>
+        </div>
+    );
+};
+
+export default Navbar;
